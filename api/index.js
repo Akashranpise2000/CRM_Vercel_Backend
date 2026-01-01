cdce2433c43a3c50bb592485aec8075d6781ce4f
Akash Ranpise <akashranpise121@gmail.com>
2026-01-01 12:07:47 +0530

    backend code

---
+++ api/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend running on Vercel");
+});
+
+module.exports = app;
